3883de724c76ae58ea6603bdcd2d27c96c64b54b
Daniel Tumelo <41048336+Mellorison@users.noreply.github.com>
2020-10-27 18:34:39 +0200

    Assignment 2

---
+++ mainone.py
@@ -0,0 +1,48 @@
+#def Music Attributes
+
+#Artist Name
+def Artist():
+	str1 = 'Sound'
+	str2 = 'Garden'
+	print('Artist: ' + str1 + str2)
+
+Artist()
+
+#Genre of Music
+def Genre():
+	str1 = 'Grunge, '
+	str2 = 'Heavy-metal, '
+	str3 = 'Alternative-metal, '
+	str4 = 'Hard-rock, '
+	str5 = 'Alternative-rock '
+	print('Genre: ' + str1 + str2 + str3 + str4 + str5)
+
+Genre()
+
+#Years Active
+def Years():
+	str1 = '1984 - 1997 . '
+	str2 = '2010 - 2019'
+	print('Years: ' + str1 + str2)
+
+Years()
+
+def functionTrue():
+	pass
+	MusicAttributes = True
+	print('Music Attributes is',bool(MusicAttributes))
+
+functionTrue()
+
+def functionFalse():
+	pass
+	MovieAttributes = False
+	print('Movie Attributes is',bool(MovieAttributes))
+
+functionFalse()
+
+
+
+
+
+ 
