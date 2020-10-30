3148eccaf26fcdb0eb27c1acfa1d3032cac8ef14
Mellorison <mellorison@gmail.com>
2020-10-30 06:16:35 +0000

    Assignment 3 Modification

--- maintwo.py
+++ maintwo.py
@@ -10,13 +10,13 @@ MusicansBand = True
 
 print(MusicansBand)
 
-if Artist == "Soundgarden":   
+if Artist == "Soundgarden":   #if the Artist name is 'Soundgarden' then print the following True output..
 
     MusicansBand = "Soundgarden is a Quartet Grunge band formed in 1984"  
 
-    YearFormed = print(int("1984"))  
+    YearFormed = print(int("1984"))  #Convert 'YearFormed' from int to string
 
-elif Artist == "Chris Cornel, Kim Thayil, Matt Cameron, Ben Shepherd":
+elif Artist == "Chris Cornel, Kim Thayil, Matt Cameron, Ben Shepherd": #if the Artisit Name is  "Chris Cornel, Kim Thayil, Matt Cameron, Ben Shepherd" the print the following true output
 
     MusicansBand = "Soundgarden is a Quartet Grunge band formed in 1984"     
 
