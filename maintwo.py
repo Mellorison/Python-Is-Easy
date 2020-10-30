0025a9a3991858332d18dacd4ad044f274e5624d
Mellorison <mellorison@gmail.com>
2020-10-30 06:05:26 +0000

    Assignment 3

--- maintwo.py
+++ maintwo.py
@@ -1,12 +1,26 @@
-Artist = 'Soundgarden'              
-SoloArtist = 'False'              
-StillActive = "Unknown"           
-MusicansBand = True                  
-print(MusicansBand)                
-if Artist == "Soundgarden":           
-    MusicansBand = "Quartet"            
-elif Artist == "Chris Cornel, Kim Thayil, Matt Cameron, Ben Shepherd":       
-    MusicansBand = "Quartet"            
+Artist = 'Soundgarden'    
+
+SoloArtist = 'False'
+
+StillActive = "Unknown"
+
+YearFormed = 1984
+
+MusicansBand = True
+
+print(MusicansBand)
+
+if Artist == "Soundgarden":   
+
+    MusicansBand = "Soundgarden is a Quartet Grunge band formed in 1984"  
+
+    YearFormed = print(int("1984"))  
+
+elif Artist == "Chris Cornel, Kim Thayil, Matt Cameron, Ben Shepherd":
+
+    MusicansBand = "Soundgarden is a Quartet Grunge band formed in 1984"     
+
 else:                          
-    MusicansBand = "Not Quartet"           
+    MusicansBand = "Not Quartet"  
+
 print(MusicansBand)               
